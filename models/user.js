020ebaf9ae47659b6f8193bbf6cf2fb6509df6bd
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-07-24 16:55:42 +0300

    feat: добавлены схемы пользователя и фильмов

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+mongoose.set('toObject', { useProjection: true });
+mongoose.set('toJSON', { useProjection: true });
+
+module.exports = mongoose.model('user', userSchema);
