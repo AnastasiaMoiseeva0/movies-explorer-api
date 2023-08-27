381990b70e0fe2fffc061e1c1315df0279fc4d52
AnastasiaMoiseeva0 <Shinaeva1997@mail.ru>
2023-08-27 14:31:58 +0300

    fix: исправлена валидация

--- middlewares/validationUser.js
+++ middlewares/validationUser.js
@@ -3,7 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const validationCreateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ tlds: false, ignoreLength: true }),
     password: Joi.string().required(),
   }),
 });
@@ -11,13 +11,13 @@ const validationCreateUser = celebrate({
 const validationUpdateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ tlds: false, ignoreLength: true }),
   }),
 });
 
 const validationLogin = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email({ tlds: false, ignoreLength: true }),
     password: Joi.string().required(),
   }),
 });
